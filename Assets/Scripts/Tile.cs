116f0fde9251b879aa59a843e946dec127002b7b
Tony <ipwnu777@gmail.com>
2019-09-30 13:12:00 +1300

    Added tile prefabs

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.UNetWeaver;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField]
+    private Biome biome;
+
+    private Tile left, right, above, below;
+    
+    private
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 size = this.gameObject.GetComponent<Renderer>().bounds.size;
+        Debug.Log(size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
