fac258b8c20933191d2eee56a3a11d4149bc4982
Andrew <andrew.h318@hotmail.co.nz>
2019-10-01 14:21:05 +1300

    resource singleton- inprogress

---
+++ Assets/Scripts/World/Resource.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace World
+{
+  public enum ResourceType
+  {
+    Money,
+    Shelter,
+    Power,
+    Food,
+    Environment
+  }
+
+
+  [System.Serializable]
+  public class Resource
+  {
+    public Resource(ResourceType type)
+    {
+      this.type = type;
+    }
+    private ResourceType type;
+    public ResourceType ResourceType => type;
+    public int MinAmount;
+    public int CurAmount;
+    public int HealthyAmount;
+  }
+}
+
---
+++ Assets/Scripts/World/ResourceEvent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceEvent
+{
+  public bool Incremented { get; set; }
+  public int Threshold { get; set; }
+
+}
