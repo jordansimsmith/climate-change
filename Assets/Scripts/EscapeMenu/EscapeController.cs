4c12f906d46e5cf06a07b3b581ac1b52ad96d126
Nisarag <nisaragb@gmail.com>
2019-10-02 13:38:13 +1300

    basic main menu implementation

---
+++ Assets/Scripts/EscapeMenu/EscapeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Net.Configuration;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscapeController : MonoBehaviour
+{
+    private static bool gameIsPaused = false;
+
+    public GameObject EscapeUIObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EscapeUIObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+
+            if (gameIsPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+        EscapeUIObj.SetActive(true);
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1f;
+        EscapeUIObj.SetActive(false);
+    }
+
+
+    public void BackButtonOnClick()
+    {
+        SceneManager.LoadScene("MainUIScene", LoadSceneMode.Single);
+    }
+}
