81f0334125fa77d415943d004efd79a04ad1a863
Jed Robertson <jrob928@aucklanduni.ac.nz>
2019-10-02 21:59:54 +1300

    Cleaned up scene and Tile class

--- Assets/Scripts/World/Tile.cs
+++ Assets/Scripts/World/Tile.cs
@@ -2,50 +2,27 @@
 
 namespace World
 {
-<<<<<<< HEAD
-  public class Tile : MonoBehaviour
-  {
-    [SerializeField] private TileType tileType;
-    public TileType TileType => tileType;
-
-    private Entity entity;
-    public Entity Entity
-    {
-      get => entity;
-      set
-      {
-        if (entity != null)
-        {
-          Destroy(entity.gameObject);
-        }
-        value.transform.SetParent(gameObject.transform, false);
-        entity = value;
-      }
-    }
-
-    public static Vector3 Size { get; } = new Vector3(10f, 2.5f, 10f);
-
-  }
-}
-=======
     public class Tile : MonoBehaviour
     {
         [SerializeField] private TileType tileType;
         public TileType TileType => tileType;
 
         private Entity entity;
-
         public Entity Entity
         {
             get => entity;
             set
             {
-                value.transform.SetParent(gameObject.transform, false);
+                if (entity != null)
+                {
+                    Destroy (entity.gameObject);
+                }
+                value.transform.SetParent (gameObject.transform, false);
                 entity = value;
             }
         }
 
-        public static Vector3 Size { get; } = new Vector3(10f, 2.5f, 10f);
+        public static Vector3 Size { get; } = new Vector3 (10f, 2.5f, 10f);
+
     }
 }
->>>>>>> feature/HUD
