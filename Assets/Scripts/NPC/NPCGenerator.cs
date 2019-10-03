f24bce665b407d63a94069cc80984665a5c637f8
res550 <37566536+res550@users.noreply.github.com>
2019-10-04 11:24:48 +1300

    Reads name data from json file

---
+++ Assets/Scripts/NPC/NPCGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NPC
+{
+    public class NPCGenerator : MonoBehaviour
+    {
+        public TextAsset nameData;
+
+        private NameData names;
+
+        private void Awake()
+        {
+            names = NameData.ParseJson(nameData.text);
+            Debug.Log(names.Names.Count);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/NPC/NameData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NPC
+{
+    [System.Serializable]
+    public class NameData
+    {
+        [SerializeField] private List<string> names;
+
+        public List<string> Names => names;
+
+        public static NameData ParseJson(string json)
+        {
+            return JsonUtility.FromJson<NameData>(json);
+        }
+    }
+}
+
