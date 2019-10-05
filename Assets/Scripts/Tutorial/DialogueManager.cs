e0d079007f02fac6bf548f040ef3ee371b5f8e79
Nisarag <nisaragb@gmail.com>
2019-10-05 16:01:40 +1300

    Merge

---
+++ Assets/Scripts/Tutorial/DialogueManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tutorial
+{
+    public class DialogueManager : MonoBehaviour
+    {
+        public Text nameText;
+        public Text dialogueText;
+
+        public Animator animator;
+
+        private Queue<string> sentences;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            sentences = new Queue<string>();
+        }
+
+
+        public void StartDialogue(Dialogue dialogue)
+        {
+
+            animator.SetBool("IsOpen", true);
+            Debug.Log("Starting conversation with " + dialogue.name);
+            nameText.text = dialogue.name;
+            sentences.Clear();
+
+            foreach (string sentence in dialogue.sentences)
+            {
+                sentences.Enqueue(sentence);
+            }
+
+            DisplayNextSentence();
+        }
+
+        public void DisplayNextSentence()
+        {
+            if (sentences.Count == 0)
+            {
+                EndDialogue();
+                return;
+            }
+
+            string sentence = sentences.Dequeue();
+            StopAllCoroutines();
+            StartCoroutine(TypeSentence(sentence));
+            Debug.Log(sentence);
+
+        }
+
+        IEnumerator TypeSentence(string sentence)
+        {
+            dialogueText.text = "";
+            foreach (var letter in sentence.ToCharArray())
+            {
+                dialogueText.text += letter;
+                yield return null;
+            }
+        }
+
+        private void EndDialogue()
+        {
+            animator.SetBool("IsOpen", false);
+            Debug.Log("End of conversation");
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorial/DialogueTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Tutorial
+{
+    public class DialogueTrigger : MonoBehaviour
+    {
+        public Dialogue dialogue;
+
+        public void TriggerDialogue()
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
+    
+    }
+}
