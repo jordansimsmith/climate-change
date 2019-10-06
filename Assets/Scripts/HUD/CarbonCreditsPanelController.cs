486dc686af8579515f126716e23d0293d6db117c
Andrew <andrew.h318@hotmail.co.nz>
2019-10-06 16:51:57 +1300

    update money per tick

--- Assets/Scripts/HUD/CarbonCreditsPanelController.cs
+++ Assets/Scripts/HUD/CarbonCreditsPanelController.cs
@@ -10,6 +10,9 @@ namespace HUD
         public Text carbonCreditsValue;
         private static CarbonCreditsPanelController _instance;
 
+        private float tempTime;
+        private float period = 1f;
+
         [SerializeField]
         private ResourceSingleton resourceSingleton;
         
@@ -22,8 +25,27 @@ namespace HUD
 
         public void Update()
         {
-            Resource resource = resourceSingleton.Money;
-            carbonCreditsValue.text = resource.CurAmount.ToString();
+            Resource money = resourceSingleton.Money;
+            tempTime += Time.deltaTime;
+
+            // always render money value
+            carbonCreditsValue.text = money.CurAmount.ToString();
+            
+            // update money with rate every second;
+            if (tempTime > period)
+            {
+                UpdateCarbonCredits(money);
+            }
+
+        }
+
+        private void UpdateCarbonCredits(Resource money)
+        {
+            tempTime = 0;
+            int rate = resourceSingleton.MoneyRate;
+            money.CurAmount += rate;
+            carbonCreditsValue.text = money.CurAmount.ToString();
+
         }
     }
 }
---
+++ Assets/Scripts/World/Entities/CarbonCreditsManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using World.Resource;
+
+namespace World.Entities
+{
+    public class CarbonCreditsManager: MonoBehaviour
+    {
+        [SerializeField] private ResourceSingleton resourceSingleton;
+
+        public void Update()
+        {
+            int rate = resourceSingleton.MoneyRate;
+            resourceSingleton.Money.CurAmount += rate;
+        }
+        
+
+    }
+}
