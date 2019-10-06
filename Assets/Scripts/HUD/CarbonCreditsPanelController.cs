f0761e9ff64bb622139916f9e11c57a179171c26
Andrew <andrew.h318@hotmail.co.nz>
2019-10-06 15:59:58 +1300

    render carbon credits controller w/ subscription to resources

---
+++ Assets/Scripts/HUD/CarbonCreditsPanelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using World.Resource;
+
+namespace HUD
+{
+    public class CarbonCreditsPanelController: MonoBehaviour
+    {
+        public Text carbonCreditsValue;
+        private static CarbonCreditsPanelController _instance;
+
+        [SerializeField]
+        private ResourceSingleton resourceSingleton;
+        
+        public static CarbonCreditsPanelController Instance => _instance;
+
+        public void Awake()
+        {
+            _instance = this;
+        }
+
+        public void Update()
+        {
+            Resource resource = resourceSingleton.Money;
+            carbonCreditsValue.text = resource.CurAmount.ToString();
+        }
+    }
+}
