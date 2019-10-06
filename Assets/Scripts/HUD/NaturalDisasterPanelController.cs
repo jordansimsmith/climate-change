185daed4c79dade48834dd37c6db807c2ecf9327
res550 <37566536+res550@users.noreply.github.com>
2019-10-06 18:32:50 +1300

    Updated controller to poll resources 3 types of disaster

--- Assets/Scripts/HUD/NaturalDisasterPanelController.cs
+++ Assets/Scripts/HUD/NaturalDisasterPanelController.cs
@@ -13,20 +13,20 @@ public class NaturalDisasterPanelController : MonoBehaviour
 
     public void Awake()
     {
-        //sub to resource singleton
-        resources.Environment.Subscribe(Callback);
+        //Every 10 Seconds check if we should dispatch an event
+        InvokeRepeating("DisasterEventDispatcher", 0,10f);
         Hide();
     }
 
-    public void Callback(ResourceEvent e)
+    public void DisasterEventDispatcher()
     {
-        // bottom 25% environment
-        if (e.WentBelow && e.Threshold == 25)
+        // bottom 25% environment not sure if this is how you should do it
+        if (resources.Environment.CurPercentage <= 25)
         {
-            // roll dice
+            // roll dice 10% to have the natural event
             if (Random.Range(0, 10) == 0)
             {
-                switch (Random.Range(0, 4))
+                switch (Random.Range(0, 5))
                 {
                     case 0:
                         Show("Sea Level Rises!",
@@ -34,19 +34,20 @@ public class NaturalDisasterPanelController : MonoBehaviour
                             "good job!");
                         return;
                     case 1:
-                        Show("Sea Level Rises!",
-                            "Your sea level rises 10% of you population now is dead" +
-                            "good job!");
+                        Show("Cyclone hits your Island!",
+                            "A cyclone ravages through your island killing people and destroying your land");
                         return;
                     case 2:
-                        Show("Sea Level Rises!",
-                            "Your sea level rises 10% of you population now is dead" +
-                            "good job!");
+                        Show("Cyclone hits your Island!",
+                            "A cyclone ravages through your island killing people and destroying your land");
                         return;
                     case 3:
-                        Show("Sea Level Rises!",
-                            "Your sea level rises 10% of you population now is dead" +
-                            "good job!");
+                        Show("There has been a drought!",
+                            "Your citizens don't have water to drink and you have lost 10% of your Islands population!");
+                        return;
+                    case 4:
+                        Show("There has been a drought!",
+                            "Your citizens don't have water to drink and you have lost 10% of your Islands population!");
                         return;
                     default:
                         return;
