8a876af8d9f28728307f1603f315174b454706e6
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-10-13 19:42:10 +1300

    Dynamically populates shop

---
+++ Assets/Scripts/HUD/ShopItem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    [SerializeField] private Text name;
+    [SerializeField] private Text cost;
+    [SerializeField] private Image image;
+
+    private Item item;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void Setup(Item item)
+    {
+        this.item = item;
+
+        // populate information views
+        name.text = item.entity.Type.ToString();
+        cost.text = "CC " + item.entity.Stats.cost;
+        image.sprite = item.sprite;
+    }
+}
---
+++ Assets/Scripts/HUD/ShopScrollList.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using World.Entities;
+
+[System.Serializable]
+public class Item
+{
+    public Entity entity;
+    public Sprite sprite;
+}
+
+public class ShopScrollList : MonoBehaviour
+{
+    [SerializeField] private List<Item> shopItems;
+    [SerializeField] private Transform contentPanel;
+    [SerializeField] private GameObject shopItemPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // populate list
+        AddItems();
+    }
+
+    private void AddItems()
+    {
+        foreach (Item item in shopItems)
+        {
+            // insantiate shop item
+            GameObject newItem = Instantiate(shopItemPrefab);
+            
+            // set parent
+            newItem.transform.SetParent(contentPanel);
+            
+            ShopItem shopItem = newItem.GetComponent<ShopItem>();
+            shopItem.Setup(item);
+        }
+    }
+}
