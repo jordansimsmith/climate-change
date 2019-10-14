d52d4f18975ac32d1f4486e4d2f305ff0338e70c
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-10-15 10:19:59 +1300

    Refactored natural disaster controller

--- Assets/Scripts/HUD/NaturalDisasterPanelController.cs
+++ Assets/Scripts/HUD/NaturalDisasterPanelController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.UI;
 using World;
-using World.Entities;
 using World.Resource;
 using World.Tiles;
 using Random = UnityEngine.Random;
@@ -23,7 +20,7 @@ public class NaturalDisasterPanelController : MonoBehaviour
     private GameObject activeTornado;
     private GameBoard board;
 
-    public void Awake()
+    private void Awake()
     {
         //Every 10 Seconds check if we should dispatch an event
         InvokeRepeating("DisasterEventDispatcher", 0, 10f);
@@ -31,11 +28,8 @@ public class NaturalDisasterPanelController : MonoBehaviour
         Hide();
     }
 
-    public void DisasterEventDispatcher()
+    private void DisasterEventDispatcher()
     {
-      
-      
-        
         // If environment drops below 100, i.e. relatively few trees to factories (you start w +300 env thanks trees).  
         var envScore = resources.totalSupply.environment - resources.totalDemand.environment;
         if (envScore < 100)
@@ -68,9 +62,8 @@ public class NaturalDisasterPanelController : MonoBehaviour
 
         //generate Rng Event for the subscribe
     }
-    
 
-    void DoCyclone()
+    private void DoCyclone()
     {
         if (activeTornado != null)
         {
@@ -78,34 +71,31 @@ public class NaturalDisasterPanelController : MonoBehaviour
         }
 
         activeTornado = Instantiate(tornadoPrefab);
-        Show("Cyclone hits your Island!",
-            "A cyclone ravages through your island killing people and destroying your land");
+        Show("Cyclone hits your island!",
+            "A climate change caused cyclone ravages your island, destroying buildings in its wake.");
     }
 
-    void DoDrought()
+    private void DoDrought()
     {
         Show("There has been a drought!",
-            "Your citizens don't have water to drink and you have lost 10% of your Islands population!");
+            "Lush fields of grass have dried out into barren desert wastelands.");
         int tilesToConvert = Random.Range(2, 6);
         ConvertTilesRandomly(TileType.Grass, TileType.Sand, tilesToConvert);
-        
     }
 
-    void DoSeaLevelRise()
+    private void DoSeaLevelRise()
     {
-        Show("Sea Level Rises!",
-            "Your sea level rises 10% of you population now is dead" +
-            "good job!"); 
-        
-        int tilesToConvert = Random.Range(2, 6);
-       ConvertTilesRandomly(TileType.Sand, TileType.Water, tilesToConvert);
+        Show("Sea levels have risen",
+            "Melting ice caps have caused the sea to erode your island's outermost regions");
 
+        int tilesToConvert = Random.Range(2, 6);
+        ConvertTilesRandomly(TileType.Sand, TileType.Water, tilesToConvert);
     }
 
-    void ConvertTilesRandomly(TileType startType, TileType endType, int numberOfTiles)
+    private void ConvertTilesRandomly(TileType startType, TileType endType, int numberOfTiles)
     {
         HashSet<Tuple<int, int>> tiles = new HashSet<Tuple<int, int>>();
-        
+
         while (numberOfTiles > 0)
         {
             Tuple<int, int> tilePos = board.GetRandomTilePosition(startType);
@@ -120,11 +110,12 @@ public class NaturalDisasterPanelController : MonoBehaviour
             Instantiate(smokePrefab, tile.gameObject.transform, false);
             tiles.Add(tilePos);
         }
-        
+
         StartCoroutine(ConvertTilesToType(tiles, endType, tileConversionDelay));
     }
 
-    IEnumerator ConvertTilesToType(HashSet<Tuple<int, int>> tiles, TileType conversionType,  float delayBeforeConversion)
+    private IEnumerator ConvertTilesToType(HashSet<Tuple<int, int>> tiles, TileType conversionType,
+        float delayBeforeConversion)
     {
         yield return new WaitForSeconds(delayBeforeConversion);
 
@@ -134,11 +125,10 @@ public class NaturalDisasterPanelController : MonoBehaviour
             Destroy(oldTile.gameObject);
             board.CreateTileAt(tile.Item1, tile.Item2, conversionType);
         }
-        
-        board.RebakeNavMesh(); // rebake nav mesh since underlying tiles have changed 
-    }
-    
 
+        // rebake nav mesh since underlying tiles have changed 
+        board.RebakeNavMesh();
+    }
 
     public void Hide()
     {
