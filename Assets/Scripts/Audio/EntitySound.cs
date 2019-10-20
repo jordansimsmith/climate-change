74cae6d51d42b69eb02c2e308f26b4e6b0778de4
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-10-20 14:55:18 +1300

    Added comments and formatted audio module

--- Assets/Scripts/Audio/EntitySound.cs
+++ Assets/Scripts/Audio/EntitySound.cs
@@ -6,13 +6,13 @@ namespace Audio
     [System.Serializable]
     public class EntitySound
     {
+        // mapping between entity type and build sound
         [SerializeField] private EntityType entityType;
         [SerializeField] private AudioClip audioClip;
-        
+
         public EntityType EntityType => entityType;
         public AudioClip AudioClip => audioClip;
 
-        [HideInInspector]
-        public AudioSource source;
+        [HideInInspector] public AudioSource source;
     }
 }
