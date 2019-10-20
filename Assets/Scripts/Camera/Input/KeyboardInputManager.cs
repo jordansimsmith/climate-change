e1aa6b20fedd43fae3ff3828164411df9ebbc89e
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-10-20 15:04:07 +1300

    Added comments to camera module

--- Assets/Scripts/Camera/Input/KeyboardInputManager.cs
+++ Assets/Scripts/Camera/Input/KeyboardInputManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class KeyboardInputManager : InputManager
 {
@@ -9,45 +7,51 @@ public class KeyboardInputManager : InputManager
     public static event ZoomInputHandler OnZoomInput;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKey(KeyCode.LeftControl) || (Input.GetKey(KeyCode.RightControl))) {
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
             return;
         }
 
-        //Handle Move
+        // Handle camera pan (WASD)
         if (Input.GetKey(KeyCode.W))
         {
             OnMoveInput?.Invoke(Vector3.forward);
         }
+
         if (Input.GetKey(KeyCode.S))
         {
             OnMoveInput?.Invoke(-Vector3.forward);
         }
+
         if (Input.GetKey(KeyCode.A))
         {
             OnMoveInput?.Invoke(-Vector3.right);
         }
+
         if (Input.GetKey(KeyCode.D))
         {
             OnMoveInput?.Invoke(Vector3.right);
         }
-        //Handle Rotation
+
+        // Handle Rotation (QE)
         if (Input.GetKey(KeyCode.Q))
         {
             OnRotateInput?.Invoke(1f);
         }
+
         if (Input.GetKey(KeyCode.E))
         {
             OnRotateInput?.Invoke(-1f);
         }
 
-
-        //Handle Zoom
+        // Handle Zoom (ZX)
         if (Input.GetKey(KeyCode.Z))
         {
             OnZoomInput?.Invoke(-1f);
         }
+
         if (Input.GetKey(KeyCode.X))
         {
             OnZoomInput?.Invoke(1f);
