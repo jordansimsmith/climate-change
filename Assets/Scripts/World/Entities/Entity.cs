69babb7cd3f76218bb74b2e70077ff697700e167
Jed Robertson <jrob928@aucklanduni.ac.nz>
2019-10-21 19:15:30 +1300

    Added ugly warning when you can't afford research

--- Assets/Scripts/World/Entities/Entity.cs
+++ Assets/Scripts/World/Entities/Entity.cs
@@ -111,9 +111,29 @@ namespace World.Entities
             {
                 research.isResearched = true;
                 return true;
+            } else {
+                // Display out of money warning
+                var HUD = GameObject.Find("HUD");
+                if (HUD)    {
+                    var warning = HUD.transform.Find("OutOfMoneyWarning");
+                    if (warning)    {
+                        warning.position = Input.mousePosition;
+                        warning.gameObject.SetActive(true);
+                        Invoke("HideCostWarning", 4);
+                    }
+                }
             }
 
             return false;
+        }
+
+        public void HideCostWarning()
+        {
+            // Hide out of money warning
+            var warning = GameObject.Find("OutOfMoneyWarning");
+            if (warning)    {
+                warning.SetActive(false);
+            }
         }
 
         private GameObject box;
